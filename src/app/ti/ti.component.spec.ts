5dcb27002814d2cc5902fb9ad1861cc4c46c8024
Arlan Vincent John German <arlangerman.senti@gmail.com>
2021-03-25 23:56:58 +0800

    During Update

--- src/app/ti/ti.component.spec.ts
+++ src/app/ti/ti.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TiComponent } from './ti.component';
 
@@ -6,7 +6,7 @@ describe('TiComponent', () => {
   let component: TiComponent;
   let fixture: ComponentFixture<TiComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TiComponent ]
     })
